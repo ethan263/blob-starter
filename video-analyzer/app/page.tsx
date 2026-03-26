0f5e05276685ff89a40506d771d3c344e18ca95c
Ethan <ethan@example.com>
2026-03-26 03:09:08 +0200

    remove placeholder content

---
+++ video-analyzer/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="relative min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-medium tracking-tight">video-analyzer</h1>
+    </main>
+  )
+}
